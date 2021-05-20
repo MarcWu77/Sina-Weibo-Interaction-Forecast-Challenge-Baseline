45cf32c6fdd162918111545283ffb7d94d7f2490
Eladio <eladiorjb@gmail.com>
2021-05-20 00:33:19 -0400

    [ADD] loader file with pandas

--- weibo_prediction/weibo_prediction.py
+++ weibo_prediction/weibo_prediction.py
@@ -1,27 +1,29 @@
 import datetime
 import string, os 
+import pandas as pd
 
+# Import Data
 curr_dir = './Weibo Data/Weibo Data/weibo_train_data(new)/'
-name = 'weibo_train_data.txt'
+name = 'weibo_train_data_aux.txt'
 fname = curr_dir+name
 file_data = []
+data = pd.read_csv(fname, delimiter = "\t", encoding="utf8", header=None) 
+"""
+    for i in range(0,len(data[0])):
+        uid = data[0][i]
+        mid = data[1][i]
+        time = data[2][i]
+        forward_count = data[3][i]
+        comment_count = data[4][i]
+        like_count = data[5][i]
+        content = data[6][i]
+"""
+# Clean the data
+# Remove mentions, punctuation, #, emojis and links
 
-with open(fname, encoding="utf8") as f: 
-    f.readline()
-    for line in f:
-        line_split = line.split()
+# Sentiment analysis
 
-        date = str(line_split[2]) 
-        time =  str(line_split[3]) 
-        date_time_str = date+' '+time
-        date_time = datetime.datetime.strptime(date_time_str, "%Y-%m-%d %H:%M:%S")
-        forward_count = line_split[4] 
-        comment_count = line_split[5] 
-        like_count = line_split[6]  
-        content = line_split[7:] 
+# Input values to the algorithm: content, keywords, sentiments, date and hour
+# LSTM network
 
-        file_data.append([date_time, int(forward_count), int(comment_count), int(like_count)]) # , str(content)])
-print(file_data)
-
-
-
+# Output count of likes, comments and forward count
